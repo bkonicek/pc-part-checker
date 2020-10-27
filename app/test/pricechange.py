74939386c801d14206545e991a761d35f2a14d6d
Benjamin Konicek <ben.konicek@gmail.com>
2020-10-27 10:28:50 -0400

    test script add list_all()

--- app/test/pricechange.py
+++ app/test/pricechange.py
@@ -1,11 +1,16 @@
 import os
+import sys
 from pymongo import MongoClient
 
 DB_HOST = os.getenv('DB_HOST', 'localhost:27017')
 
 
-def main():
+def create_client():
     client = MongoClient('mongodb://%s' % DB_HOST)
+    return client
+
+
+def price_change(client):
     db = client.parts
     collection = db[db.list_collection_names()[0]]
     part = collection.find_one()
@@ -13,8 +18,22 @@ def main():
                           '$set': {'price': part['price'] + 10}})
     print('Updated %s\'s price from %s to %s' %
           (part['name'], part['price'], (part['price'] + 10)))
-    client.close()
+
+
+def list_all(client):
+    db = client.parts
+    collection = db[db.list_collection_names()[0]]
+    for part in collection.find():
+        print(part)
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) > 2:
+        print('Invalid number of arguments. Use either `pricechange` or `dump`')
+        sys.exit(1)
+    client = create_client()
+    if sys.argv[1] == 'pricechange' or len(sys.argv) == 1:
+        price_change(client)
+    elif sys.argv[1] == 'dump':
+        list_all(client)
+    client.close()
